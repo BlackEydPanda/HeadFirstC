d6aa399fc2615535f9f15e69c633b869e59f68a5
BlackEydPanda <cunotip@gmail.com>
2026-04-05 22:58:14 +0200

    Add playground

---
+++ playground/dobule_2_int.c
@@ -0,0 +1,9 @@
+// This file is to test what happenst when you cast double to int
+#include <stdio.h>
+
+int main() {
+  printf("%i", (int)0.1);
+  printf("%i", (int)0);
+  printf("%i", (int)-0.1);
+  return 0;
+}
